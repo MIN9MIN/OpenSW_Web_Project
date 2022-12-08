3af2ebb0d3d815ee3a364fcd4967a9ca8b99fbc4
Kazin7 <krustm8@naver.com>
2022-12-08 21:40:28 +0900

    DB Connect

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
 app.use(express.static('./'));
+const bodyParser= require('body-parser')
+app.use(express.urlencoded({ extended: true }));
+const MongoClient = require('mongodb').MongoClient
 
-app.listen(8090, function () {
+var db;
+MongoClient.connect('mongodb+srv://hunterspin:a862842@cluster0.qr6s3gn.mongodb.net/?retryWrites=true&w=majority', function(err, client){
+    if (err) return console.log(err)
+    db = client.db('member');
+    console.log('DB Connect');
+    
+    app.listen(8090, function () {
     console.log('listening on 8090')
+    })
 })
 
 app.get('/', function (req, res) {
@@ -28,6 +38,16 @@ app.get('/service', function (req, res) {
 
 app.get('/login', function (req, res) {
     res.sendFile(__dirname + '/login.html')
+})
+
+app.post('/add', function(req, res){
+    res.send('complete....')
+    db.collection('login').insertOne({id:req.body.id , email:req.body.email, password: req.body.password}, function(err, result){
+      console.log("save complete...");
+      console.log(req.body.id);
+      console.log(req.body.email);
+      console.log(req.body.password);
+    })
 })
 
 app.get('/signUp', function (req, res) {
