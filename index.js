6bb5a519dc754350e3112f501308fa71c6e3d5d8
MIN9MIN <43162547+MIN9MIN@users.noreply.github.com>
2022-12-08 17:41:20 +0900

    Add files via upload
    first_upload front-end

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+app.use(express.static('./'));
+
+app.listen(8090, function () {
+    console.log('listening on 8090')
+})
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/introduce', function (req, res) {
+    res.sendFile(__dirname + '/introduce.html')
+})
+
+app.get('/notice', function (req, res) {
+    res.sendFile(__dirname + '/notice.html')
+})
+
+app.get('/board', function (req, res) {
+    res.sendFile(__dirname + '/board.html')
+})
+
+app.get('/service', function (req, res) {
+    res.sendFile(__dirname + '/service.html')
+})
