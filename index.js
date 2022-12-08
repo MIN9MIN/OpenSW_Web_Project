a15e523f4f62a04befa923866165e994c731a72d
Kazin7 <krustm8@naver.com>
2022-12-09 03:14:15 +0900

    check id and password

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ MongoClient.connect('mongodb+srv://hunterspin:a862842@cluster0.qr6s3gn.mongodb.n
     if (err) return console.log(err)
     db = client.db('member');
     console.log('DB Connect');
-    
+
     app.listen(8090, function () {
     console.log('listening on 8090')
     })
@@ -41,15 +41,37 @@ app.get('/login', function (req, res) {
 })
 
 app.post('/add', function(req, res){
-    res.send('complete....')
-    db.collection('login').insertOne({id:req.body.id , email:req.body.email, password: req.body.password}, function(err, result){
-      console.log("save complete...");
-      console.log(req.body.id);
-      console.log(req.body.email);
-      console.log(req.body.password);
+    db.collection('login').findOne({id:req.body.id} ,function(err, result){
+        if(result == null){
+            db.collection('login').insertOne({id:req.body.id , email:req.body.email, password: req.body.password}, function(err, result){
+                console.log("save complete...");
+                console.log(req.body.id);
+                console.log(req.body.email);
+                console.log(req.body.password);
+            })
+            res.sendFile(__dirname + '/index.html');
+        }
+        else{
+            res.send("<script>alert('이미 존재하는 아이디입니다.');history.go(-1);</script>");
+        }
     })
 })
 
+app.post('/find', function(req, res){
+    db.collection('login').findOne({id:req.body.id} ,function(err, result) {
+        if (err) throw err;
+        if(result == null){
+            res.send("<script>alert('아이디 혹은 비밀번호가 틀렸거나 존재하지 않습니다.');history.go(-1);</script>");
+        }
+        else if(result.password == req.body.password){
+            res.sendFile(__dirname + '/index.html');
+        }
+        else{
+            res.send("<script>alert('아이디 혹은 비밀번호가 틀렸거나 존재하지 않습니다.');history.go(-1);</script>");
+        }
+      })
+})
+
 app.get('/signUp', function (req, res) {
-    res.sendFile(__dirname + '/signUp.html')
+    res.sendFile(__dirname + '/signUp.html');
 })
